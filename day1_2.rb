e44381e596ad4545c4fefd58b76367142bb78e31
Harper <harper@barnett.work>
2021-12-01 15:33:47 +0000

    Updated to work over thinternet!

--- day1_2.rb
+++ day1_2.rb
@@ -1,6 +1,11 @@
+require 'mechanize'
 def main
-    file = File.open("day1.txt")
-    file_data = file.readlines.map(&:chomp)
+    agent = Mechanize.new
+    agent.cookie_jar.load("cookies.txt", :cookiestxt)
+    file = agent.get("https://adventofcode.com/2021/day/1/input").body
+    #puts file.body
+    #file = File.open("day1.txt")
+    file_data = file.split("\n")
     total = 0
     for i in (3..file_data.length()) do
         if file_data[i].to_i > file_data[i-3].to_i
