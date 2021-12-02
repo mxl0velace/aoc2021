31df38e8ec90407c59bb0e5eae78b3ceb1d4e5aa
Harper <harper@barnett.work>
2021-12-02 11:50:45 +0000

    Day 2

---
+++ day2_2.rb
@@ -0,0 +1,34 @@
+require 'mechanize'
+def main
+    agent = Mechanize.new
+    agent.cookie_jar.load("cookies.txt", :cookiestxt)
+    file = agent.get("https://adventofcode.com/2021/day/2/input").body
+    file_data = file.split("\n")
+    hor = 0
+    ver = 0
+    aim = 0
+
+    file_data.each {
+        |line|
+        ls = line.split(" ")
+        case ls[0]
+        when "forward"
+            hor += ls[1].to_i
+            ver += ls[1].to_i * aim
+        when "down"
+            aim += ls[1].to_i
+        when "up"
+            aim -= ls[1].to_i
+        else
+            puts "Error!"
+        end
+    }
+
+    puts "Hor = #{hor}"
+    puts "Ver = #{ver}"
+    puts "Multiplied = #{hor*ver}"
+end
+
+if __FILE__ == $0
+    main
+end
