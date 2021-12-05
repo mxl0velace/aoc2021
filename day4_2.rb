4451a85a3d50777d9f5704a2d1a4127662215699
Harper <harper@barnett.work>
2021-12-05 14:05:29 +0000

    Day 4

---
+++ day4_2.rb
@@ -0,0 +1,119 @@
+require './template.rb'
+file_data = loadlines(4)
+$gameOver = false
+
+
+class Row
+    def initialize(nums)
+        @nums = nums
+    end
+
+    def mark(val)
+        @nums.delete(val)
+        return @nums.length() == 0
+    end
+end
+
+def genRows(board)
+    length = board.length()
+    n = 0
+    rows = Array.new
+    while n < length
+        rows << Row.new(board[n..n+4])
+        n += 5
+    end
+    return rows
+end
+
+def genCols(board)
+    length = board.length() / 5
+    n = 0
+    cols = Array.new
+    while n < length
+        cols << Row.new([board[n], board[n+5], board[n+(5*2)], board[n+(5*3)], board[n+(5*4)]])
+        n += 1
+    end
+    return cols
+end
+
+class BingoBoard
+    def initialize(board)
+        @board = board
+        @rows = genRows(board)
+        @cols = genCols(board)
+        @permaboard = board
+        @haswon = false
+    end
+
+    def mark(num)
+        @board.delete(num)
+        @rows.each{
+            |row|
+            if row.mark(num)
+                self.win(num)
+            end
+        }
+
+        @cols.each{
+            |col|
+            if col.mark(num)
+                self.win(num)
+            end
+        }
+        return @haswon
+    end
+
+    def win(num)
+        total = 0
+        @board.each{
+            |val|
+            total += val
+        }
+        puts "Winner: #{total * num}"
+        @haswon = true
+    end
+end
+
+bingoList = file_data.shift
+bList = Array.new
+bingoList.split(",").each{
+    |item|
+    iitem = item.to_i
+    bList << iitem 
+}
+
+len = file_data.length()
+n = 0
+allBoards = Array.new
+
+while n < len
+    b = Array.new
+    file_data[n+1..n+5].each {
+        |line|
+        ls = line.split
+        ls.each {
+            |item|
+            b << item.to_i
+        }
+    }
+    allBoards << BingoBoard.new(b)
+    n += 6
+end
+
+puts allBoards
+
+n = 0
+
+while not allBoards.length == 0
+    puts "Marking #{bList[n]}"
+    allBoards.delete_if{
+        |board|
+        m = board.mark(bList[n])
+        if m
+            puts "Something should be removed"
+        end
+        m
+    }
+    puts "Remaining: #{allBoards.length}"
+    n += 1
+end
